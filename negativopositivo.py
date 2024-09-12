6c1eef103fdc800a0d0b18f01714c58060584e81
Hudson678 <Hudsonmores31@gmail.com>
2024-09-12 13:02:30 -0300

    Negativo e positivo

---
+++ negativopositivo.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+numero = int(input("digite um numero:"))
+if numero > 0:
+    print ("positivo")
+else:
+        if numero == 0:
+            print("neutro")
+if numero < 0:
+    print ("negativo")
